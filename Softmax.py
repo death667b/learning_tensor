aae7566dca10a1bb7a99970560f31d5f4d26a7f1
Ian Daniel <idaniel@spinnys.com>
2016-09-11 15:18:57 +1000

    softmax rocks

--- Softmax.py
+++ Softmax.py
@@ -1,17 +1,24 @@
 
-
-scores = [3.0, 1.0, 0.2]
-
 import numpy as np 
+scores = np.array([[1, 2, 3, 6],
+		   			[2, 4, 5, 6],
+		   			[3, 8 ,7, 6]])
+
+
 
 def softmax(x):
 	"""Compute softmax values for each sets of scores in x."""
-	return (np.exp(x)) / (np.sum(np.exp(x)))
+	return np.exp(x) / np.sum(np.exp(x), axis=0)
 
 print (softmax(scores))
 
 # Plot softmax curves
 import matplotlib.pyplot as plt 
+x = np.arange(-2.0, 6.0, 0.1)
+score = np.vstack([x, np.ones_like(x), 0.2 * np.ones_like(x)])
 
-plt.plot([1.0, 2.0, 3.0], softmax(scores), linewidth=2)
+print (x.shape)
+print (score.shape)
+
+plt.plot(x, softmax(score/10).T, linewidth=2)
 plt.show()
